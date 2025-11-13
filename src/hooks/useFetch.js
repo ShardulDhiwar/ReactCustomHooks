ed59394dcfbafc8e2aa7732044b7069a48a6adcf
ShardulDhiwar <sujaldhiwar007@gmail.com>
2025-11-13 20:52:46 +0530

    Update useFetch.js

--- src/hooks/useFetch.js
+++ src/hooks/useFetch.js
@@ -2,7 +2,7 @@ import { useState, useEffect, useCallback } from "react";
 
 const useFetch = (url) => {
     const [data, setData] = useState(null);
-    const [loading, setLoading] = useState(true);
+    const [loading, setLoading] = useState(false);
     const [error, setError] = useState(null);
 
     const fetchData = useCallback(async () => {
